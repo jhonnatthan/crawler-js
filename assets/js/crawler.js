6c41c361174d9d25656c13ece0c23d4f74201fa1
jhonnatthan <jhonnatthans@outlook.com>
2019-05-23 22:04:51 -0300

    [ADD] - Estrutrura de Dados & Função de Iniciação & Crawler do Cinemark

--- assets/js/crawler.js
+++ assets/js/crawler.js
@@ -1,3 +1,39 @@
+let films = {
+    cineRoxy: [],
+    cineMark: [],
+    espacoFilmes: []
+};
+
+window.onload = function () {
+    startApp();
+}
+
+function startApp() {
+    getCineMark();
+}
+
+function getCineMark() {
+    let url = 'https://www.cinemark.com.br';
+    startRequest(url + '/santos/filmes/em-cartaz', {
+        html: true,
+        callBack: (dom) => {
+
+            let cartazes = dom.querySelectorAll('.movie-container');
+
+            cartazes.forEach(cartaz => {
+                let title = cartaz.querySelector('.movie-title a').innerText.trim();
+                let image = url + cartaz.querySelector('source').getAttribute('srcset');
+
+                films.cineMark.push({ title, image });
+            });
+
+            console.log(films);
+        }
+    });
+
+}
+
+
 function startRequest(url, opts) {
     let xhr = new XMLHttpRequest();
 
