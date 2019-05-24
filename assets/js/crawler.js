a953f2b3e3f781ebc5c2ad0d3452470a81103d48
jhonnatthan <jhonnatthans@outlook.com>
2019-05-23 22:03:00 -0300

    [ADD] - Função de Request/Crawling

---
+++ assets/js/crawler.js
@@ -0,0 +1,17 @@
+function startRequest(url, opts) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (opts.html) {
+                opts.callBack(parseDOM(this.responseText));
+            } else {
+                opts.callBack(this.responseText);
+            }
+        }
+    };
+
+    xhr.open("GET", "https://cors-anywhere.herokuapp.com/" + url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+}
